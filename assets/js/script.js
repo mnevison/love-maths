27dad32aa8c80e3b96169d7875a580e3421b67a9
mnevison <mnevison2@gmail.com>
2024-04-13 03:46:47 +0100

    Update: updated division logic to round down to the nearest whole number for answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,9 @@ function runGame(gameType) {
 
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
+  
+  let num4 = Math.floor(Math.random() * 9) + 1;
+  let num3 = num4 * Math.floor(Math.random() * 10) + 1;
 
 
 
@@ -46,7 +49,7 @@ function runGame(gameType) {
   } else if (gameType === "multiply") {
     displayMultiplyQuestion(num1, num2);
   } else if (gameType === "division") {
-    displayDivisionQuestion(num1, num2);
+    displayDivisionQuestion(num3, num4);
   } else {
     alert(`Unknown Game Type: ${gameType}`);
     throw `Unknown Game Type: ${gameType}, Aborting!`;
@@ -90,7 +93,7 @@ function calculateCorrectAnswer() {
   } else if (operator === "x") {
     return [operand1 * operand2, "multiply"];
   } else if (operator === "/") {
-    return [operand1 % operand2, "division"];
+    return [Math.floor(operand1 / operand2), "division"];
   } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting! `;
@@ -136,7 +139,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-  document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-  document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
   document.getElementById("operator").textContent = "/";
 }
